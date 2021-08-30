105da567ffdf1f1e33b8d8fe396c43223fb97ed1
karthik123-hash <karthikdputhran@gmail.com>
2021-08-30 19:43:10 +0530

    First commit

---
+++ src/reducer.js
@@ -0,0 +1,34 @@
+export const initialState={
+    basket: [],
+ }
+ //selector
+export const getBasketTotal =(basket)=>
+basket?.reduce((amount,item)=>item.price + amount, 0)
+ const reducer = (state,action)=>{
+     console.log(action)
+     switch(action.type){
+         case 'ADD_TO_BASKET':
+             return{
+                 ...state,
+                 basket:[...state.basket,action.item],
+             };
+        case 'REMOVE_FROM_BASKET':
+             const index=state.basket.findIndex(
+                 (basketItem)=> basketItem.id === action.id
+             );
+             let newBasket=[...state.basket];
+
+             if(index >= 0){
+                newBasket.splice(index,1);//removes the product based on index or position
+             }else{
+                 console.warn(`cant remove product(id: ${action.id} ) as its not in basket:`)
+             }
+             return{
+                 ...state,
+                 basket: newBasket
+             }
+             default:
+                 return state;
+     }
+ }
+ export default reducer
